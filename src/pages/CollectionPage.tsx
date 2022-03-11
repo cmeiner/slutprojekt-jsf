02e5fd3a1a012b2425cc4be628bc8d84bdedcdfd
felixloot <felix.bakkum2002@gmail.com>
2022-03-11 12:59:48 +0100

    ID HANTERING
    URL id passerar in och hittar rätt collection med hjälp utav UseParams()

--- src/pages/CollectionPage.tsx
+++ src/pages/CollectionPage.tsx
@@ -1,18 +1,31 @@
-import { CSSProperties, useState } from "react";
+import { CSSProperties, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import ProductCard from "../components/card";
 import { collectionData } from "../data/collections/collection";
 
+
+
 function CollectionPage() {
+ // Använder ID från routern (:id) lägger det i en variabel
+  const {id} = useParams()
+
+  // Hämtar hem den orgienlla listan för sedan kunna leta igenom den
   const [collectionList, setCollectionList] = useState(collectionData);
 
+  // Letar i colllection listan efter id som matchar URL routerns ID och sedan sätter det objectet till "Collection"
+  const [collection, setCollection] = useState(collectionList.find(p => p.id.toString() === id));
 
-  // collectionData.find(collection.id === 1)
-  // findCollection.nft.map()
+  // Sedar passar vi in "nft" då i varje productCard som är NFTSEN :)
+
+
+  useEffect(() => {
+    console.log(collection)
+  }, [])
 
   return (
     <div style={flexProducts}>
-      {collectionList.map((collection, index) => (
-        <ProductCard collection={false} key={index} card={collection} />
+      {collection?.NFTS.map((nft, index) => (
+        <ProductCard collection={true} key={index} card={collection} />
       ))}
     </div>
   );
