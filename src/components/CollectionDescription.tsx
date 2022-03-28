ac1c2a225549437de395b7398400b439b1ff5a98
miMeiner <christian.meiner@medieinstitutet.se>
2022-03-28 12:19:20 +0200

    more fixes

--- src/components/CollectionDescription.tsx
+++ src/components/CollectionDescription.tsx
@@ -1,14 +1,22 @@
+import { CSSProperties } from "react";
 import { collectionData } from "../data/collections/collection";
 import { useProducts } from "./context/ProductContext";
 
 
 function CollectionDescription() {
     const { collections } = useProducts()
-    return <div>
-        <div>
-            <h2>Fis</h2>
+    return <div style={{display: "flex", justifyContent: "center"}}>
+        <div style={CollectionDescriptionContainer}>
+            <h2>{collections.}</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam dolorem consectetur eius, molestias corporis quidem fugiat odit incidunt cumque tenetur! Dicta, quia sunt tempora qui neque laboriosam labore debitis? Repellat.</p>
         </div>
     </div>
 }
 
 export default CollectionDescription
+
+const CollectionDescriptionContainer: CSSProperties = {
+display: "flex",
+justifyContent: "center",
+width: "50%",
+}
